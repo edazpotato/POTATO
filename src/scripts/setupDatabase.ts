100dc3493d956c026265c8c3c148abbc1a291e93
Edazpotato <edazpotato@gmail.com>
2021-11-06 10:25:59 +1300

    database setup for analytics
    Signed-off-by: Edazpotato <edazpotato@gmail.com>

--- src/scripts/setupDatabase.ts
+++ src/scripts/setupDatabase.ts
@@ -16,5 +16,57 @@ openDatabase().then(async (db) => {
 		console.info('Error creating table "guilds".');
 	}
 
+	try {
+		await db.exec(`
+		CREATE TABLE "stats_system" (
+			"id"	INTEGER NOT NULL UNIQUE,
+			"timestamp"	INTEGER NOT NULL,
+			"memory_use"	INTEGER NOT NULL,
+			"memoray_total"	INTEGER NOT NULL,
+			"cpu_use"	INTEGER NOT NULL,
+			"cpu_total"	INTEGER NOT NULL,
+			"bandwidth_use"	INTEGER NOT NULL,
+			PRIMARY KEY("id" AUTOINCREMENT)
+		);
+		`);
+		console.info('Created table "stats_system".');
+	} catch (e) {
+		console.info('Error creating table "stats_system".');
+	}
+
+	try {
+		await db.exec(`
+		CREATE TABLE "stats_bot" (
+			"id"	INTEGER NOT NULL UNIQUE,
+			"timestamp"	INTEGER NOT NULL,
+			"guilds"	INTEGER NOT NULL,
+			"users"	INTEGER NOT NULL,
+			"shards"	INTEGER NOT NULL,
+			"clusters"	INTEGER NOT NULL,
+			PRIMARY KEY("id" AUTOINCREMENT)
+		);
+		`);
+		console.info('Created table "stats_bot".');
+	} catch (e) {
+		console.info('Error creating table "stats_bot".');
+	}
+
+	try {
+		await db.exec(`
+		CREATE TABLE "stats_bot_command_invocations" (
+			"id"	INTEGER NOT NULL UNIQUE,
+			"timestamp"	INTEGER NOT NULL,
+			"guild_id"	TEXT,
+			"user_id"	TEXT,
+			"command_name"	TEXT NOT NULL,
+			"sub_command_name"	TEXT,
+			PRIMARY KEY("id" AUTOINCREMENT)
+		);
+		`);
+		console.info('Created table "stats_bot_command_invocations".');
+	} catch (e) {
+		console.info('Error creating table "stats_bot_command_invocations".');
+	}
+
 	return;
 });
