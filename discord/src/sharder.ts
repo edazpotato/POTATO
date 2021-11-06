8fec8815dba854c88e46acacf8eb255bfcfa0f5f
Edazpotato <edazpotato@gmail.com>
2021-11-06 18:20:10 +1300

    fix logging
    Signed-off-by: Edazpotato <edazpotato@gmail.com>

--- discord/src/sharder.ts
+++ discord/src/sharder.ts
@@ -32,54 +32,6 @@ if (!TOKENS.HYPIXEL)
 
 const developmentMode = !!TOKENS.TESTING_GUILD_ID;
 
-if (!developmentMode || true) {
-	openDatabase().then((db) => {
-		// Need to run this before anything else so that it can being calculating properly
-		si.networkStats().then(() => {
-			// let usedBytes = 0;
-			setInterval(async () => {
-				const memory = await si.mem();
-
-				const cpuLoad = Math.round(
-					(await si.currentLoad()).currentLoad,
-				);
-
-				const netStats = await si.networkStats("*");
-				// if (usedBytes <= 0)
-				// 	usedBytes = netStats.reduce(
-				// 		(prev, current) => prev + current.rx_bytes,
-				// 		0,
-				// 	);
-				const usedBytesLatest = netStats.reduce(
-					(prev, current) => prev + current.rx_bytes,
-					0,
-				);
-				const bandwidth = usedBytesLatest; // - usedBytes;
-				// usedBytes = usedBytesLatest;
-
-				console.log({
-					$time: Math.floor(Date.now()),
-					$bandwidth: bandwidth,
-					$cpu_load: cpuLoad,
-					$memory_use: memory.used,
-					$memory_total: memory.total,
-				});
-
-				// db.run(
-				// 	"INSERT INTO stats_system (timestamp, bandwidth_use, cpu_load, memory_use, memory_total) VALUES $time, $bandwidth, $cpu_load, $memory_use, $memory_total",
-				// 	{
-				// 		$time: Math.floor(Date.now()),
-				// 		$bandwidth: bandwidth,
-				// 		$cpu_load: cpuLoad,
-				// 		$memory_use: memory.used,
-				// 		$memory_total: memory.total,
-				// 	},
-				// );
-			}, 5 * 1000);
-		});
-	});
-}
-
 const clusterCount = developmentMode ? 2 : undefined;
 const shardCount = developmentMode ? 2 : 2;
 
@@ -89,6 +41,54 @@ if (isPrimary) {
 	} else {
 		log("No TESTING_GUILD_ID provided. Bot is in production mode.");
 	}
+
+	openDatabase().then((db) => {
+		// Need to run this before anything else so that it can being calculating properly
+		si.networkStats().then(() => {
+			let usedBytes = 0;
+			setInterval(async () => {
+				const memory = await si.mem();
+
+				const cpuLoad = Math.round(
+					(await si.currentLoad()).currentLoad,
+				);
+
+				const netStats = await si.networkStats("*");
+				if (usedBytes <= 0)
+					usedBytes = netStats.reduce(
+						(prev, current) => prev + current.rx_bytes,
+						0,
+					);
+				const usedBytesLatest = netStats.reduce(
+					(prev, current) => prev + current.rx_bytes,
+					0,
+				);
+				const bandwidth = usedBytesLatest - usedBytes;
+				usedBytes = usedBytesLatest;
+
+				if (developmentMode) {
+					console.log({
+						$time: Math.floor(Date.now()),
+						$bandwidth: bandwidth,
+						$cpu_load: cpuLoad,
+						$memory_use: memory.used,
+						$memory_total: memory.total,
+					});
+				}
+
+				db.run(
+					"INSERT INTO stats_system (timestamp, bandwidth_use, cpu_load, memory_use, memory_total) VALUES $time, $bandwidth, $cpu_load, $memory_use, $memory_total",
+					{
+						$time: Math.floor(Date.now()),
+						$bandwidth: bandwidth,
+						$cpu_load: cpuLoad,
+						$memory_use: memory.used,
+						$memory_total: memory.total,
+					},
+				);
+			}, 60 * 60 * 1000); // Every hour
+		});
+	});
 }
 
 const sharder = new ShardingManager(join(__dirname, "shard"), {
